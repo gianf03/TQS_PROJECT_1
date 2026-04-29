4241123ecb783e423152fc2d014dbb88da824c4a
gianf03 <gianfvitiello03@gmail.com>
2026-04-29 22:32:24 +0200

    adeguata la versione classica

--- classic_key_recovery_attack.py
+++ classic_key_recovery_attack.py
@@ -1,9 +1,12 @@
 from sdes import generate_keys, encrypt, decrypt
 import bitarray
 import time
+import os
 
 
 def brute_force(plaintext, ciphertext):
+
+    keyCounter = 0
     
     for key in range(0, 1024):
         bitKey = bin(key)[2:].zfill(10)
@@ -15,32 +18,39 @@ def brute_force(plaintext, ciphertext):
 
         if (decryption == plaintext):
             print("Chiave usata:\t", bitKey)
-            #return 
+            keyCounter += 1
+
+
+    print(f"\nTotale chiavi:\t {keyCounter}")
+
+    return keyCounter
 
 
 
-key = bitarray.bitarray("0011100000")
-plaintext = bitarray.bitarray("00000000")
-
-subKey1, subKey2 = generate_keys(key)
-
-ciphertext = bitarray.bitarray("11111110")
-#ciphertext = encrypt(plaintext, subKey1, subKey2)
-
-print("\n")
-print("Plaintext:\t", plaintext.to01())
-#print("Master key:\t", key.to01())
-print("Ciphertext:\t", ciphertext.to01())
-#print("Plaintext:\t", decrypt(ciphertext, subKey1, subKey2).to01())
-
-print("\n")
-start = time.perf_counter_ns()
-brute_force(plaintext, ciphertext)
-end = time.perf_counter_ns()
+#plaintext = bitarray.bitarray("10101010")
+#ciphertext = bitarray.bitarray("00000111")
 
 
-print("\n")
-elapsed_time = end - start
-print(elapsed_time, " ns")
+for p in range(256):
+    for c in range(256):
+        plaintext = bitarray.bitarray(f"{p:08b}")
+        ciphertext = bitarray.bitarray(f"{c:08b}")
+        # fai qualcosa con plaintext e ciphertext
+
+        print("\n")
+        print("Plaintext:\t", plaintext.to01())
+        print("Ciphertext:\t", ciphertext.to01())
+
+        print("\n")
+        start = time.perf_counter_ns()
+        keyCounter = brute_force(plaintext, ciphertext)
+        end = time.perf_counter_ns()
+
+        elapsed_time = end - start
+        print(f"\n{elapsed_time} ns")
+        
+        if keyCounter == 16:
+            os._exit(0)
+
 
 
